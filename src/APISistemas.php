f67166508e3666f0a791ba1a18fa7253fd3bce43
Claudson Martins <claudson@outlook.com>
2018-09-03 16:15:03 -0300

    Adicionado suporte a query strings

--- src/APISistemas.php
+++ src/APISistemas.php
@@ -15,6 +15,12 @@ class APISistemas
 
     protected $httpClient;
 
+    /**
+     * Cria uma nova instância da APISistemas.
+     *
+     * @param  string  $accessToken
+     * @param  bool  $development
+     */
     public function __construct(string $accessToken, bool $development = false)
     {
         $this->accessToken = $accessToken;
@@ -23,23 +29,37 @@ class APISistemas
         ]);
     }
 
-    public function get(string $path)
+    /**
+     * Faz a requisição ao caminho informado, podendo utilizar query strings.
+     *
+     * @param  string  $path
+     * @param  array|null  $query
+     * @return mixed|string
+     */
+    public function get(string $path, array $query = null)
     {
         try {
-            $response = $this->httpClient->request('GET', $path, [
-                'headers' => [
-                    'Authorization' => 'Bearer '.$this->accessToken,
-                    'Accept' => 'application/json',
-                ],
-            ]);
+            $options['headers'] = [
+                'Authorization' => 'Bearer '.$this->accessToken,
+                'Accept' => 'application/json',
+            ];
+            if (! empty($query)) {
+                $options['query'] = $query;
+            }
+            $response = $this->httpClient->request('GET', $path, $options);
             $content = $response->getBody()->getContents();
 
             return json_decode($content);
         } catch (GuzzleException $e) {
-            var_dump($e);
+            return $e->getMessage();
         }
     }
 
+    /**
+     * Realiza uma requisição ao endpoint que retorna os dados do usuário.
+     *
+     * @return mixed|string
+     */
     public function self()
     {
         $response = $this->get('usuario');
--- tests/index.php
+++ tests/index.php
@@ -3,6 +3,7 @@ require '../vendor/autoload.php';
 
 $api = new UFS\APISistemas(env('ACESS_TOKEN'), true);
 $result = $api->self();
+$result = $api->get('departamentos', ['limit' => 10]);
 
 header('Content-Type: application/json');
 echo json_encode($result);
